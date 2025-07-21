c4daf37e439a027ef1963e33b7c60db823976331
Anurag Ganvir <anuragganvir2019@gmail.com>
2025-07-21 09:54:42 +0530

    added structure for components pricing and education

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 function Education() {
     return ( 
-        <h1>Education</h1>
+        <div className="container mt-9">
+            <div className="row">
+                 <div className="col-6">
+                    <img src="media/images/education.svg" style={{width:"70%"}} alt="" />
+                </div>
+                <div className="col-6">
+                    <h1 className='mb-3 fs-2'>Free and Open Market education</h1>
+                    <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
+                    <a href="" style={{textDecoration:"none"}}>Versity</a>
+
+                    <p className='mt-5'>TradingQ&A, the most active trading and investment community in India for all your market related queries.</p>
+                    <a href="" style={{textDecoration:"none"}}>Trading Q&A</a>
+                </div>                
+            </div>
+        </div>
      );
 }
 
--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 function Pricing() {
     return ( 
-        <h1>Pricing</h1>
+        <div className="container mb-5">
+            <div className="row">
+                <div className="col-4">
+                    <h1 className=' mb-3 fs-2'>Unbeatable pricing</h1>
+                    <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                    <a href="" style={{textDecoration:"none"}}>See pricing</a>
+                </div>
+                <div className="col-2"></div>
+                <div className="col-6">
+                    <div className="row text-center">
+                        <div className="col p-3 border">
+                            <h1 className='mb-2'>₹0</h1>
+                            <p>Free equity delivery and <br />direct mutual funds</p>
+                        </div>
+                        <div className="col p-3 border">
+                            <h1 className='mt-2'>₹20</h1>
+                            <p className='mb-5'>Intraday and F&O</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
      );
 }
 
