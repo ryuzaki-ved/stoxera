e78a65a6761c4ed815dedf095143811912e08a8d
Anurag Ganvir <anuragganvir2019@gmail.com>
2025-07-22 16:33:19 +0530

    updated the props from Product page and left and right section

--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -19,6 +19,21 @@ function ProductsPage() {
         appStore=""
       />
       <RightSection
+        imageURL="media/images/console.png"
+        productName="Console"
+        productDesription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+        learnMore=""
+      />
+      <LeftSection
+        imageURL="media/images/coin.png"
+        productName="Coin"
+        productDesription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection
         imageURL="media/images/kiteconnect.png"
         productName="Kite Connect API"
         productDesription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
@@ -33,6 +48,10 @@ function ProductsPage() {
         googlePlay=""
         appStore=""
       />
+      <p className="text-center mt-5 mb-5">
+        Want to know more about our technology stack? Check out the Zerodha.tech
+        blog.
+      </p>
       <Universe />
     </>
   );
